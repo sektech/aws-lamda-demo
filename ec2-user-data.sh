2e24df7fede2dcc2424a5a0851ab392cad819d89
Sekar R <98270894+sektech@users.noreply.github.com>
2022-07-27 21:21:15 +0100

    Update ec2-user-data.sh

--- ec2-user-data.sh
+++ ec2-user-data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo yum update -y
 sudo yum install -y httpd
-sudo systemctl start httpd
-sudo systemctl enable httpd
+sudo systemctl start httpd.service
+sudo systemctl enable httpd.service
 echo "<h1> Hello world from $(hostname -f)</h1>" > /var/www/html/index.html
